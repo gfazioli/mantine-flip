d606d8a6c380930ab85d32362adcb70131b70546
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-02-05 12:58:40 +0100

    fix: selector

--- docs/styles-api/Flip.styles-api.ts
+++ docs/styles-api/Flip.styles-api.ts
@@ -4,11 +4,15 @@ import type { StylesApiData } from '../components/styles-api.types';
 export const FlipStylesApi: StylesApiData<FlipFactory> = {
   selectors: {
     root: 'Root element',
+    "flip-inner": "Inner element",
+    "flip-content": "Content element"
   },
 
   vars: {
     root: {
-      '--test-component-color': 'Controls root element `background-color`',
+      "--flip-perspective": "Controls animation `perspective`",
+      "--flip-transition-duration": "Controls animation `duration`",
+      "--flip-transition-timing-function": "Controls animation `easing`"
     },
   },
 
