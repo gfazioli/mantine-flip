5fbb4afadea36370374f423a8ab5cab70fcb66bb
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2024-02-06 15:07:33 +0100

    Updates

--- _next/static/DJeCHJ3GTVATmE5jmjTxR/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/9793-e4347b89dcc8b6b9.js","static/css/ee445a7f61bd9536.css","static/chunks/pages/index-901b834f6cde1720.js"],"/_error":["static/chunks/pages/_error-78f2934ae700e086.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/Mgtirp6D9YZx_iky1HCEE/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/7691-f671f1426856fef2.js","static/css/411d08967b80a6bc.css","static/chunks/pages/index-24e98261751c53b6.js"],"/_error":["static/chunks/pages/_error-78f2934ae700e086.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
