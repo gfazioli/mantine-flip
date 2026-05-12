4d742c9e2d6efc56ccfb2d5068fc953898f27f49
Giovambattista Fazioli <giovambattista.fazioli@gmail.com>
2026-05-12 15:40:07 +0200

    Updates

---
+++ _next/static/chunks/0~svki1p7oheg.js
@@ -0,0 +1,11 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/061sbpk9koopf.js",
+  "static/chunks/0lz_bbex6gt0m.js",
+  "static/chunks/01bdl3s_m~26j.js",
+  "static/chunks/0k_xyv3y691_q.js",
+  "static/chunks/0ao7pgm8wxsrm.js",
+  "static/chunks/0wul7izdk-~t9.css",
+  "static/chunks/0odi0wnxvrxb3.css",
+  "static/chunks/0dbfwy~x8swne.css",
+  "static/chunks/turbopack-052n8sapqff91.js"
+])
---
+++ _next/static/wQV71q-MbYc5g1BVkolCs/_buildManifest.js
@@ -0,0 +1,18 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/10gp3~e-cf35v.js"
+  ],
+  "/_error": [
+    "static/chunks/0jqz~a0ry1l_p.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
